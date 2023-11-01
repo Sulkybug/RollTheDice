e8fd675a8cd8b2476eee39cb39f42df0efc01130
Sulkybug <feang_c0@hotmail.com>
2023-11-01 02:12:47 -0500

    Dice Img update

--- src/Components/Dices.jsx
+++ src/Components/Dices.jsx
@@ -1,14 +1,14 @@
 const dices = {
-  "/img/Numb1.png": 1,
-  "/img/Numb2.png": 2,
-  "/img/Numb3.png": 3,
-  "/img/Numb4.png": 4,
-  "/img/Numb5.png": 5,
-  "/img/Numb6.png": 6,
-  "/img/FourDiceNum1.png": 1,
-  "/img/FourDiceNum2.png": 2,
-  "/img/FourDiceNum3.png": 3,
-  "/img/FourDiceNum4.png": 4,
+  "/RollTheDice/img/Numb1.png": 1,
+  "/RollTheDice/img/Numb2.png": 2,
+  "/RollTheDice/img/Numb3.png": 3,
+  "/RollTheDice/img/Numb4.png": 4,
+  "/RollTheDice/img/Numb5.png": 5,
+  "/RollTheDice/img/Numb6.png": 6,
+  "/RollTheDice/img/FourDiceNum1.png": 1,
+  "/RollTheDice/img/FourDiceNum2.png": 2,
+  "/RollTheDice/img/FourDiceNum3.png": 3,
+  "/RollTheDice/img/FourDiceNum4.png": 4,
 };
 
 export default dices;
